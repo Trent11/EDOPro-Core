edo9300/ygopro-core

commit afee133db2bac2cea579d2cd582b57c8c280f039

Remove unnecessary face-up check in card::get_link_marker
Closes #114

card.cpp
@@ -1396,8 +1396,6 @@ uint32 card::get_rscale() {
 	return rscale;
 }
 uint32 card::get_link_marker() {
-	if(!(current.position & POS_FACEUP))
-		return 0;
 	if (assume.find(ASSUME_LINKMARKER) != assume.end())
 		return assume[ASSUME_LINKMARKER];
 	if(!(get_type() & TYPE_LINK))

Trent11/EDOPro-Core

commit a92da806d7481fea07b380c1e56782a32440a6cc

Update get_link_marker to work with the Extra deck
Because  the function `IsLinkMarker` uses `get_link_marker `, it fails for a link monster in the Extra deck, that will be face-down.

Merge #114
Fix regression introduced in afee133db2bac2cea579d2cd582b57c8c280f039

card.cpp
@@ -1396,6 +1396,8 @@ uint32 card::get_rscale() {
 	return rscale;
 }
 uint32 card::get_link_marker() {
+	if(!(current.position & POS_FACEUP) && (!current.is_location(LOCATION_EXTRA)))
+		return 0;
 	if (assume.find(ASSUME_LINKMARKER) != assume.end())
 		return assume[ASSUME_LINKMARKER];
 	if(!(get_type() & TYPE_LINK))